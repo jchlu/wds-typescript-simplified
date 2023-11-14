4a1c599717f6fa710acc86bc3b32b33efba790ba
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-14 12:09:44 -0400

    Define types

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
+type Person = {
+  name: string
+  age: number
+  isProgrammer?: boolean
+  address?: {
+    houseNumber: number
+    street: string
+  }
+}
+
+const personA: Person = {
+  name: "Johnny",
+  age: 99,
+  isProgrammer: true,
+}
+
+const personB: Person = {
+  name: "Zuby",
+  age: 21,
+  address: {
+    houseNumber: 45,
+    street: 'Main Street',
+  },
+}
+
 console.log('Hi from Vite')
 let a: number = 4
 
 const b = [1, 2, 3]
 
-b.push(4) 
+b.push(4)
 
 
