a722d96bca6eec5a39dac75278800144f56dd33a
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-14 12:39:23 -0400

    void keyword versus undefined

--- src/main.ts
+++ src/main.ts
@@ -25,12 +25,14 @@ const personB: Person = {
   },
 }
 
-function printPerson(person: { name: string }) {
+// The void return type is infered here as there is never anything returned.
+// Add a 'return' keyword without anything after it also imlies a void function return.
+function printPerson(person: { name: string }): void {
   console.log(`Hey there ${person.name}`)
 }
 
 printPerson({ name: 'Johnny' })
 // Next call errors because you're explicitly setting a variable that doesn't exist on the function
-printPerson({ name: 'Stanley', age: 88 }) 
+// printPerson({ name: 'Stanley', age: 88 }) 
 // Whereas:
 printPerson(personB) // Works just fine
