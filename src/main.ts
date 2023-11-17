045d828db1ea6dbfb4e0c2d81c60b53d13e7fd16
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-17 12:11:10 -0400

    Without a generic any is implied

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,16 @@ const input = document.querySelector<HTMLInputElement>('.input')
 /* Might return a value or undefined */
 console.log(input?.value)
 /* All is good */
+
+type Person = {
+  name: string
+  age: number
+}
+
+function getSecondElement(array) {
+/* Parameter 'array' implicitly has an 'any' type. typescript (7006) [12, 27] */
+  return array[1]
+}
+
+const arrayA = [1, 2, 3]
+const arrayB = [{name: 'Fred', age: 44}, {name: 'Bill', age: 66}]
