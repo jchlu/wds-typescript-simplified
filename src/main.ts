f02d36a6e0359f244499796adb180295b2845f11
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-17 16:02:45 -0400

    Complex example of generic use after failing JS

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ type Person = {
 }
 
 function getSecondElement<ArrayType>(array: ArrayType[]): ArrayType {
-/* Parameter 'array' implicitly has an 'any' type. typescript (7006) [12, 27] */
+  /* Parameter 'array' implicitly has an 'any' type. typescript (7006) [12, 27] */
   return array[1]
 }
 
 const arrayA = [1, 2, 3]
-const arrayB = [{name: 'Fred', age: 44}, {name: 'Bill', age: 66}]
+const arrayB = [{ name: 'Fred', age: 44 }, { name: 'Bill', age: 66 }]
 
 const returnA = getSecondElement(arrayA)
 const returnB = getSecondElement(arrayB)
@@ -23,9 +23,31 @@ const returnB = getSecondElement(arrayB)
 /* Typescript can imply the return without implicitly defining them */
 
 // A functio to convery and array of tuples to an object
-function aToO(arr){
-  const obj = {}
-  arr.forEach(([key, value]) => {
-    obj[key] = [value]
+function aToO<T>(array: [string, T][]) {
+  const obj: {
+    [index: string]: T
+  } = {}
+
+  array.forEach(([key, value]) => {
+    obj[key] = value
   })
+
+  return obj
 }
+
+const arrA: [string, string | number | boolean][] = [
+  ["key1", 1],
+  ["key2", 'Gravy'],
+  ["key3", true]
+]
+
+const arrB: [string, string | number][] = [
+  ["key1", 1],
+  ["key2", "sausages"],
+  ["key3", 4]
+]
+const objA = aToO(arrA)
+const objB = aToO(arrB)
+
+console.log(`objA: ${JSON.stringify(objA, null, 4)}`)
+console.log(`objB: ${JSON.stringify(objB, null, 4)}`)
