8faccba9d8f2599439ccb2d4522e60cb9dab8c18
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-18 12:16:23 -0400

    Demo of Omit / Pick

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
-function wait(delay: number) {
-  return new Promise<string>(resolve => {
-    setTimeout(() => resolve('Sausages'), delay)
-  })
+type Todo = {
+  id: string
+  title: string
+  completed: boolean
 }
 
-wait(1000).then(value => {
-  console.log(value.length)
-})
+// strip off id with Omit to allow setting on save
+type NewTodo = Omit<Todo, 'id'>
+type NewTodoUsingPick = Pick<Todo, "title" |"completed">
+const todoA: NewTodo = {
+  title: 'Cook Sausages',
+  completed: false
+}
+
+function saveTodo(todo: NewTodo): Todo {
+  /*   write to database for example */
+  return {...todo, id: crypto.randomUUID()}
+}
+
+function renderTodo(todo: Todo) {
+  const div = document.createElement('div')
+  div.id = todo.id
+  div.innerHTML = todo.title
+  const app = document.querySelector<HTMLDivElement>('#app')
+  app?.append(div)
+}
+
+const savedTodo = saveTodo(todoA)
+renderTodo(savedTodo)
