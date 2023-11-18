128b0ed447b89f458804160fd78bec7859e2d41f
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-18 12:36:08 -0400

    Examples using Required/ Partial

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,34 @@
 type Todo = {
   id: string
   title: string
+  description?: string
   completed: boolean
 }
 
+// Examples of Required and Partial
+type StrictTodo = Required<Todo>
+/* type StrinctTodo = {
+    id: string;
+    title: string;
+    description: string;
+    completed: boolean;
+} */
+
+type OtherTodo = Partial<Todo>
+/* type OtherTodo = {
+    id?: string | undefined;
+    title?: string | undefined;
+    description?: string | undefined;
+    completed?: boolean | undefined;
+} */
+
+// Use in combination with Pick / Omit to create type with a single source of truth
+
 // strip off id with Omit to allow setting on save
 type NewTodo = Omit<Todo, 'id'>
+// created new type by picking only what you need (for different reasons)
 type NewTodoUsingPick = Pick<Todo, "title" |"completed">
+
 const todoA: NewTodo = {
   title: 'Cook Sausages',
   completed: false
