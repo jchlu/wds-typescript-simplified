796abbf147279117fd6bf8c47bf27b70ef094632
Johnny Choudhury-Lucas <client-werewolf-insulin@exito.tech>
2023-11-19 12:24:24 -0400

    using Record

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
+// Example of using the Record type
+type Person = {
+  name: string
+  age: number
+}
+
+type PeopleGroupedByName1 = { [index: string]: Person[] }
+/* Can be rewritten usinf Record: */
+
+type PeopleGroupedByName2 = Record<string, Person[]>
+/* or, more spefifically if required: */
+
+type PeopleGroupedByName3 = Record<Person["name"], Person[]>
+
 const checkLength = (a: string, b: number) => {
   return a.length < b // true or false
 }
